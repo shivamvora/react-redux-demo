614151803ae4009bf7ffb1052e7266b735253b0b
shivam <shivampatel27102001@gmail.com>
2021-02-16 23:29:59 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [counter,setCounter] = useState(0);
+
+    
   return (
     <div className="App">
           
-    </div>
+          <h1 onClick={()=>setCounter(counter + 1)}> 
+            
+            Counter : {counter}
+            
+            </h1>
+          
+      </div>
   );
 }
 
